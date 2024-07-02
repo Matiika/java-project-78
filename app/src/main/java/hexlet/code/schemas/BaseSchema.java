0300b4f510b8f4f4b4f742a55d45c12abefe2ea7
Матвей <matvei@MacBook-Pro-Matvej.local>
2024-07-03 00:58:53 +0300

    Update

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -20,6 +20,13 @@ public abstract class BaseSchema<T> {
 
     /*public abstract boolean isValid(Object value);*/
 
+
+    /**
+     * Checks if the value is valid.
+     * Subclasses can override this method to provide specific validation logic.
+     *
+     * @return true if the schema is valid, false otherwise
+     */
     public boolean isValid(Object value) {
 
         for (Predicate<Object> check : checks.values()) {
